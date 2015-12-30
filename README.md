d08701cf9a852c3f8bfa34cc4db017ee688aa83f
Paul Taylor <pftaylor61@gmail.com>
2015-12-30 15:01:54 -0600

    Commit First

---
+++ README.md
@@ -0,0 +1,15 @@
+![ocws-options-export-import](./assets/castlelogo80x80.png)
+
+# OCWS Options Export and Import
+This plugin will take a look at your theme, whether it be a parent or child theme, and will read the options settings for it. These options settings can then be saved to your computer. In reverse, you can upload an options settings file, in order to restore options to your theme.
+
+I have to confess that I did very little to create this plugin, but since the code is open source, and since I find it convenient for it to be in a plugin form, I am offering it.
+
+## Credits
+The idea came from the website [Digging Into Wordpress](https://digwp.com/2014/04/backup-restore-theme-options/), and was compiled by Jeff Starr. However, he, in turn, got the idea from the [Gantry Export and Import Options](https://digwp.com/2014/04/backup-restore-theme-options/) plugin. According to the plugin details, that plugin uses the code exclusively for his themes' framework. My plugin should work with any theme, that uses an options system. All the themes, with which I have so far tried this plugin, use the [Options Framework](http://wptheming.com/options-framework-theme/).
+
+## Version 1.0
+I edited the code for 2 reasons:
+
+1. So that the code works with child themes.
+2. In order to ensure that the code does not clash, even if you are using a theme that already has this options exporting class in it.
---
+++ ocws-options-export-import.php
@@ -0,0 +1,133 @@
+<?php
+
+/*
+
+Plugin Name: OCWS Options Export and Import
+Plugin URI: http://oldcastleweb.com/pws/plugins
+
+Description: This plugin will add a system for backing up options settings in your theme. The plugin has been produced by <a href="http://www.oldcastleweb.com" target="_blank">Old Castle Web Solutions</a>.<br /><br /> A panel is added, which reads the options settings in the current theme, and enables them to be saved or imported.
+
+Version: 1.0
+Author: Paul Taylor
+Author URI: http://oldcastleweb.com/pws/about
+License: GPL2
+GitHub Plugin URI: https://github.com/pftaylor61/ocws-options-export-import
+GitHub Branch:     master
+
+*/
+
+/*  Copyright 2015  Paul Taylor  (email : info@oldcastleweb.com)
+
+
+
+    This program is free software; you can redistribute it and/or modify
+    it under the terms of the GNU General Public License, version 2, as 
+    published by the Free Software Foundation.
+
+    This program is distributed in the hope that it will be useful,
+    but WITHOUT ANY WARRANTY; without even the implied warranty of
+    MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+    GNU General Public License for more details.
+
+    You should have received a copy of the GNU General Public License
+    along with this program; if not, write to the Free Software
+    Foundation, Inc., 51 Franklin St, Fifth Floor, Boston, MA  02110-1301  USA
+
+*/
+
+/* Section to save options */
+/*
+	Backup/Restore Theme Options
+	@ https://digwp.com/2014/04/backup-restore-theme-options/
+	Go to "Appearance > Backup Options" to export/import theme settings
+	(based on "Gantry Export and Import Options" by Hassan Derakhshandeh)
+
+	Usage:
+	1. Add entire backup/restore snippet to functions.php
+	2. Edit 'shapeSpace_options' to match your theme options
+*/
+if (!class_exists(backup_restore_theme_options)) {
+class backup_restore_theme_options {
+
+	function backup_restore_theme_options() {
+		add_action('admin_menu', array(&$this, 'admin_menu'));
+	}
+	function admin_menu() {
+		// add_submenu_page($parent_slug, $page_title, $menu_title, $capability, $menu_slug, $function);
+		// $page = add_submenu_page('themes.php', 'Backup Options', 'Backup Options', 'manage_options', 'backup-options', array(&$this, 'options_page'));
+
+		// add_theme_page($page_title, $menu_title, $capability, $menu_slug, $function);
+		$page = add_theme_page('Backup Options', 'Backup Options', 'manage_options', 'backup-options', array(&$this, 'options_page'));
+
+		add_action("load-{$page}", array(&$this, 'import_export'));
+	}
+	function import_export() {
+                $ocwsqt_option_name = get_option( 'stylesheet' );
+		if (isset($_GET['action']) && ($_GET['action'] == 'download')) {
+			header("Cache-Control: public, must-revalidate");
+			header("Pragma: hack");
+			header("Content-Type: text/plain");
+			header('Content-Disposition: attachment; filename="'.$ocwsqt_option_name.'-options-'.date("dMy").'.dat"');
+			echo serialize($this->_get_options());
+			die();
+		}
+		if (isset($_POST['upload']) && check_admin_referer('shapeSpace_restoreOptions', 'shapeSpace_restoreOptions')) {
+			if ($_FILES["file"]["error"] > 0) {
+				// error
+			} else {
+				$options = unserialize(file_get_contents($_FILES["file"]["tmp_name"]));
+				if ($options) {
+					foreach ($options as $option) {
+						update_option($option->option_name, unserialize($option->option_value));
+					}
+				}
+			}
+			wp_redirect(admin_url('themes.php?page=backup-options'));
+			exit;
+		}
+	}
+	function options_page() { ?>
+
+		<div class="wrap">
+			<?php screen_icon(); ?>
+			<h2>Backup/Restore Theme Options</h2>
+			<form action="" method="POST" enctype="multipart/form-data">
+				<style>#backup-options td { display: block; margin-bottom: 20px; }</style>
+				<table id="backup-options">
+					<tr>
+						<td>
+							<h3>Backup/Export</h3>
+							<p>Here are the stored settings for the current theme:</p>
+							<p><textarea class="widefat code" rows="20" cols="100" onclick="this.select()"><?php echo serialize($this->_get_options()); ?></textarea></p>
+							<p><a href="?page=backup-options&action=download" class="button-secondary">Download as file</a></p>
+						</td>
+						<td>
+							<h3>Restore/Import</h3>
+							<p><label class="description" for="upload">Restore a previous backup</label></p>
+							<p><input type="file" name="file" /> <input type="submit" name="upload" id="upload" class="button-primary" value="Upload file" /></p>
+							<?php if (function_exists('wp_nonce_field')) wp_nonce_field('shapeSpace_restoreOptions', 'shapeSpace_restoreOptions'); ?>
+						</td>
+					</tr>
+				</table>
+			</form>
+		</div>
+
+	<?php }
+	function _display_options() {
+		$options = unserialize($this->_get_options());
+	}
+	function _get_options() {
+		global $wpdb;
+                
+                $ocwsqt_option_name = get_option( 'stylesheet' );
+                
+		return $wpdb->get_results("SELECT option_name, option_value FROM {$wpdb->options} WHERE option_name = '".$ocwsqt_option_name."'"); // edit 'qohelet' to match theme options
+	}
+}
+}
+if (class_exists(backup_restore_theme_options)){
+    new backup_restore_theme_options();
+}
+/* End of options saving section */
+
+?>
